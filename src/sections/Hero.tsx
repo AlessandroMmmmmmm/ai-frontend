27916974fad699f5aec71b06d60efc16b5a491be
Alessandro Montesi <alessandromontesi234@gmail.com>
2024-10-27 15:21:37 -0700

    logoticker and features

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,14 +2,17 @@ import {Button} from '@/components/button';
 import starsBg from "@/assets/stars.png";
 export const Hero = () => {
   return(
-  <section className="h-[492px] flex items-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]" 
+  <section className="h-[492px] md:h-[800px] flex items-center overflow-hidden relative [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]" 
   style={{
     backgroundImage: `url(${starsBg.src})`,
   }}>
     <div className="absolute inset-0 bg-[radial-gradient(75%_75%_at_center_center,rgb(140,69,255,0.5)_15%,rgb(14,0,36,0.5)_78%,transparent)]"></div>
+   {/*Start Planet */}
     <div className="absolute h-64 w-64 bg-purple-500 rounded-full border border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
     bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))] 
     shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,0.1),0_0_50px_rgb(140,69,255)]"></div>
+    {/*End Planet */}
+    
     {/*RINGS */}
     <div className="absolute h-[344px] w-[344px] border rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-20 ">
       {/* <div className="absoulute h-2 w-2 left-0 bg-white rounded-full top-1/2 -translate-x-1/2  -translate-y-1/2"></div>
@@ -23,9 +26,9 @@ export const Hero = () => {
     
     
     <div className="container relative mt-16">
-      <h1 className="text-8xl font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">AI SEO</h1>
-      <p className="text-lg text-white/70 mt-5 text-center">
-        Elevate your site&apos;s visibilitytools with AI, where smart 
+      <h1 className="text-8xl md:text-[168px] md:leading-nonefont-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">AI SEO</h1>
+      <p className="text-lg text-white/70 mt-5 text-center max-w-xl mx-auto">
+        Elevate your site&apos;s visibility tools with AI, where smart 
         technology meets user-friendly SEO.
       </p>
       <div className="flex justify-center mt-5">
