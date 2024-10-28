59111d86e020da4fd3d661bf0557055bb83acf47
Alessandro Montesi <alessandromontesi234@gmail.com>
2024-10-27 19:32:17 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import {twMerge} from "tailwind-merge";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "AI Startup Landing Page",
-  description: "A landing page for an AI startup created with Frontend Tribe",
+  title: "AI SEO Startup",
+  description: "An AI SEO startup company",
 };
 
 export default function RootLayout({
